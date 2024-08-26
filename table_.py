801e29ffba517145c252c22fc50d848661a27001
CodeWithRizvi <codewithrizvi317@gmail.com>
2024-08-26 08:40:04 +0500

    Add files via upload

---
+++ table_.py
@@ -0,0 +1,14 @@
+             ## Sample
+n=int(input("Enter a number:"))
+for i in range(10):
+    print(n,"x",i+1,"=",n*(i+1))
+
+
+
+
+
+
+              ## Use f string
+n=int(input("Enter a number :"))
+for i in range(1,11):
+    print(f"{n} X {i} = {n*i}")
