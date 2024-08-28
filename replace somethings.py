b28f9268679f07cd02e401c33e34092fb0d931ca
CodeWithRizvi <codewithrizvi317@gmail.com>
2024-08-28 08:36:06 +0500

    Delete replace somethings.py

--- replace somethings.py
+++
@@ -1,8 +0,0 @@
-letter = ''' 
-Dear <|Name|>,
-You are selected!
-<|Date|>
-'''
-# Use the 'replace' method to insert the name and date into the letter
-print(letter.replace("<|Name|>","Muhammad Abdullah Rizvi").replace("<|Date|>","8/24/2024"))
-
